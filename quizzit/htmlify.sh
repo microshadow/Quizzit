18b4fd0f6f9061dde923042ae8baa759118cb245
ahurka <hurka.alex@gmail.com>
2018-11-04 12:11:24 -0500

    Create preprocessor script

---
+++ quizzit/htmlify.sh
@@ -0,0 +1,21 @@
+echo "Removing CSS imports..."
+
+mkdir rmv
+for filename in src/*.js; do
+    cat ${filename} | grep -v -e .css -e .png > rmv/`basename ${filename}`
+done
+
+echo "Preprocessing JSX..."
+cp jsx.babelrc .babelrc
+TIMEOUT=4
+
+# Timeout the JSX processor after TIMEOUT seconds
+( npx babel --watch rmv --out-dir build --presets react-app/prod ) & pid=$!
+( sleep ${TIMEOUT} && kill -HUP ${pid} ) 2>/dev/null & watcher=$!
+wait ${pid} 2>/dev/null && kill -HUP ${watcher}
+
+rm -rf rmv
+
+echo "Bunding import statements..."
+cp require.babelrc .babelrc
+browserify build/index.js -o build/quizzit.js
