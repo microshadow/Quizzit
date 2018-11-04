9067884abebf0e3e9d8c1fa76e47d25d4bb93b66
ahurka <hurka.alex@gmail.com>
2018-11-04 12:10:51 -0500

    Init template HTML and script

---
+++ quizzit/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const rootElem = (
+  <div className='replace'>
+    Replace Me
+  </div>
+);
+
+ReactDOM.render(rootElem, document.getElementById('root'));
