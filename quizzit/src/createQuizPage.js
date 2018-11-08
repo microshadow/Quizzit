ca4e732e1a6478f032e94639415e07fed9fc4677
Jonathan Lehner <lehnerj@student.ethz.ch>
2018-11-08 15:26:44 -0500

    new quiz component finished

--- quizzit/src/createQuizPage.js
+++ quizzit/src/createQuizPage.js
@@ -53,7 +53,6 @@ export class CreateQuizPage extends React.Component {
     }
 
     addQuiz(title){
-        console.log(title);
         let new_quizzes_array = this.state.quizzes;
         new_quizzes_array.push({title: title, questions: []});
         this.setState({quizzes: new_quizzes_array});
@@ -87,13 +86,10 @@ class CreatequizForm extends React.Component {
     }
 
     setTitle(newtitle){
-        console.log(newtitle);
         this.setState({title: newtitle});
     }
 
     render(){
-        console.log("title");
-        console.log(this.state.title);
         return (
             <div>
                 <Form.Group as={Row} controlId="formHorizontalTitle">
@@ -119,49 +115,102 @@ class CreatequizForm extends React.Component {
 class CreatequestionForm extends React.Component{
     constructor(props){
         super(props);
-        this.state = {selectedIndex: -1, selectedQuiz: 0}
+        this.state = {
+            selectedIndex: -1, 
+            selectedQuiz: 0, 
+            selectedCheckbox: -1,
+            questionTitle: "",
+            choiceValues: ["","","",""],
+            quizzes: props.quizzes,
+        }
     }
 
     changeActiveQuestion(index){
-        this.setState({selectedIndex: index})
+        if(index == -1){ 
+            //changing to "add question" mode, reset the "selected question" index
+            // and the selected checkbox
+            this.setState({selectedIndex: index, selectedCheckbox: index});
+        }
+        else{
+            const activeQuiz = this.props.quizzes[this.state.selectedQuiz];
+            if(activeQuiz == null) return;
+            const activeIndex = activeQuiz.questions[index].correct_index;
+            this.setState({selectedIndex: index, 
+                selectedCheckbox: activeIndex, 
+                questionTitle: activeQuiz.questions[index].question,
+                choiceValues: activeQuiz.questions[index].choices});
+        }
     }
 
     deleteQuestion(index){
-
+        const selectedQuiz = this.state.selectedQuiz;
+        const activeQuestionIndex = this.state.selectedIndex;
+        let new_quizzes_array = this.props.quizzes;
+        new_quizzes_array[selectedQuiz].questions[activeQuestionIndex].splice(index, 1);
+        this.setState({quizzes: new_quizzes_array});
     }
 
     addQuestion(event){
         event.preventDefault();
+        const selectedQuiz = this.state.selectedQuiz;
+        let new_quizzes_array = this.props.quizzes;
+        const new_question = {
+            question: this.state.questionTitle,
+            choices: this.state.choiceValues,
+            correct_index: this.state.selectedCheckbox,
+            }
+        new_quizzes_array[selectedQuiz].questions.push(new_question)
+        this.setState({quizzes: new_quizzes_array, choiceValues: ["","","",""], questionTitle: ""});
     }
 
     modifyQuestion(event, index){
-        let form = event.target;
-        console.log(form);
+        event.preventDefault();
+        const selectedQuiz = this.state.selectedQuiz;
+        const activeQuestionIndex = this.state.selectedIndex;
+        let new_quizzes_array = this.props.quizzes;
+        const new_question = {
+            question: this.state.questionTitle,
+            choices: this.state.choiceValues,
+            correct_index: this.state.selectedCheckbox,
+            }
+        new_quizzes_array[selectedQuiz].questions[activeQuestionIndex] = new_question;
+        this.setState({quizzes: new_quizzes_array});
     }
 
     changeActiveQuiz(event){
         this.setState({selectedQuiz: event.target.value})
     }
 
+    handleSelect(event){
+        //need to set selectedCheckbox index to an integer value
+        this.setState({selectedCheckbox: event.target.value*1})
+    }
+
+    handleChangeTitle(event){
+        this.setState({questionTitle: event.target.value})
+    }
+
+    handleChangeChoice(event, index){
+        let choiceValues = this.state.choiceValues;
+        choiceValues[index] = event.target.value;
+        this.setState({choiceValues: choiceValues})
+    }
+
     render(){
         const selectedQuiz = this.state.selectedQuiz;
         const activeQuestionIndex = this.state.selectedIndex;
         const hasActiveQuestion = (activeQuestionIndex !== -1);
-        const activeQuiz = hasActiveQuestion ? this.props.quizzes[selectedQuiz] : null;
-        console.log(this.state.selectedIndex);
-        console.log(hasActiveQuestion);
-        console.log(this.props.quizzes)
-        console.log(activeQuiz);
+        const activeQuiz = this.props.quizzes[selectedQuiz];
         return(
         <Form>
             <Form.Group as={Row} controlId="formGridState">
                 <Form.Label column sm={2}>
                     Quiz
                 </Form.Label>
-                <Form.Control as="select" sm={5} bsPrefix="select_quiz" onChange={(event, index) => {this.changeActiveQuiz(event);}}>
+                <Form.Control as="select" sm={5} bsPrefix="select_quiz" value={selectedQuiz} onChange={(event, index) => {this.changeActiveQuiz(event);}}>
                     {this.props.quizzes.map((quiz, index) => {
                         return (
-                            <option value={index} key={index} active={index == selectedQuiz}>{quiz.title}</option>
+                            <option value={index} key={index}>{quiz.title}</option>
                             )
                         })
                     }
@@ -172,7 +221,7 @@ class CreatequestionForm extends React.Component{
                     Questions
                 </Form.Label>
                 <QuestionsList 
-                    questions={this.props.quizzes[selectedQuiz].questions} 
+                    questions={activeQuiz.questions} 
                     changeActiveQuestion={this.changeActiveQuestion.bind(this)}
                     deleteQuestion={this.deleteQuestion.bind(this)}
                     />
@@ -182,17 +231,22 @@ class CreatequestionForm extends React.Component{
                     <b>{(!hasActiveQuestion) ? "Add" : "Modify"} question</b>
                 </Form.Label>
                 {(hasActiveQuestion) ? 
-                    <Form.Label column sm={4}>
-                        {this.props.quizzes[selectedQuiz].questions[activeQuestionIndex].question}
+                    <Form.Label column sm={6}>
+                       <Form.Control onChange={(event)=>{this.handleChangeTitle(event)}} 
+                            value={this.state.questionTitle} 
+                            placeholder={this.props.quizzes[selectedQuiz].questions[activeQuestionIndex].question}/>
                     </Form.Label>
-                : ""}
+                :   <Form.Label column sm={4}>
+                        <Form.Control onChange={(event)=>{this.handleChangeTitle(event)}} 
+                            value={this.state.questionTitle} 
+                            placeholder="type question"/>
+                    </Form.Label>
+                }
                 {(hasActiveQuestion) ? 
                     <Form.Label column sm={2}>
                         <a id="changeMode_link" onClick={()=>{this.changeActiveQuestion(-1)}}>Add new question instead</a>
                     </Form.Label>
-                :   <Form.Label column sm={4}>
-                        <Form.Control placeholder="type question"/>
-                    </Form.Label>
+                : ""
                 }
 
             </Form.Group>
@@ -201,16 +255,13 @@ class CreatequestionForm extends React.Component{
                 ["A","B","C","D"].map((letter, index) => {
 
                     const choices = hasActiveQuestion ? activeQuiz.questions[activeQuestionIndex].choices : [];
-                    console.log(index);
-                    console.log("hasActiveQuestion");
-                    console.log(hasActiveQuestion);
                     return (
-                        <div>
-                            <Form.Group key={index} as={Col} controlId="formGridZip">
-                                <Form.Label>{"choice "+letter}</Form.Label>
-                                <Form.Control placeholder={hasActiveQuestion ? choices[index] : "type choice"} />
-                            </Form.Group>
-                        </div>
+                        <Form.Group key={index} as={Col} controlId="formGridZip">
+                            <Form.Label>{"choice "+letter}</Form.Label>
+                            <Form.Control onChange={(event)=>{this.handleChangeChoice(event, index)}}
+                                value={this.state.choiceValues[index]}
+                                placeholder={hasActiveQuestion ? choices[index] : "type choice"} />
+                        </Form.Group>
                     )
                 })
             }
@@ -223,16 +274,16 @@ class CreatequestionForm extends React.Component{
             <Col sm={10}>
                 {
                     ["A","B","C","D"].map((letter, index) => {
-                        const activeIndex = hasActiveQuestion ? (activeQuiz.questions[activeQuestionIndex].correct_index) : -1;
-                        console.log(activeIndex);
                         return (
                             <Form.Check
                                 key={index}
+                                value={index}
                                 type="radio"
                                 label={"choice "+letter}
                                 name="formHorizontalRadios"
                                 id="formHorizontalRadios1"
-                                checked={index == activeIndex}
+                                checked={index === this.state.selectedCheckbox}
+                                onChange={(event) => {this.handleSelect(event);}}
                             />
                             )
                     })
@@ -240,7 +291,9 @@ class CreatequestionForm extends React.Component{
             </Col>
             </Form.Group>
         </fieldset>
-            <Button variant="primary" type="submit" onClick={(event)=>this.addQuestion(event)}>
+            <Button variant="primary" 
+                type="submit" 
+                onClick={hasActiveQuestion ? (event)=>this.modifyQuestion(event) : (event)=>this.addQuestion(event)}>
                 {hasActiveQuestion ? "Modify" : "Add"} question
             </Button>
         </Form>
@@ -249,7 +302,12 @@ class CreatequestionForm extends React.Component{
 
 class QuestionsList extends React.Component{
     render(){
-        console.log(this.props.questions);
+        if(this.props.questions.length == 0){
+            return (
+                <ListGroup>
+                    <ListGroup.Item key={0}>No questions added</ListGroup.Item>
+                </ListGroup>);
+        }
         return (
             <ListGroup>
                 {this.props.questions.map((question, index) => {
