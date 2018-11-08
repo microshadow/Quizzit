5d7414dcd4b9f11da86538797898e6a7e954362c
anshayghosh <anshay.ghosh@mail.utoronto.ca>
2018-11-08 03:30:16 -0500

    adding routing and first screens

---
+++ quizzit/src/router.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import WelcomeScreen from "./welcomeScreen.js";
+import LogInScreen from "./logInScreen.js";
+import Dashboard from "./dashboard.js";
+import SignUpScreen from "./signUpScreen.js";
+import { STUDENT } from "./globals.js";
+
+class Router extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={WelcomeScreen}/>
+                    <Route path="/logIn" exact component={LogInScreen}/>
+                    <Route path="/signUp" exact component={SignUpScreen}/>
+                    <Route path="/dashboard" exact render={()=><Dashboard userType={STUDENT}/>} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default Router;
--- quizzit/src/welcomeScreen.js
+++ quizzit/src/welcomeScreen.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class WelcomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.goToLogIn = this.goToLogIn.bind(this);
+        this.goToSignUp = this.goToSignUp.bind(this);
+    }
+
+    goToLogIn() {
+        this.props.history.push("/logIn");
+    }
+
+    goToSignUp() {
+        this.props.history.push("/signUp");
+    }
 
     render() {
         return (
             <div>
                 <h1>Welcome to Quizzit</h1>
-                <button>Log In</button>
-                <button>Sign Up</button>
+                <button onClick={this.goToLogIn}>Log In</button>
+                <button onClick={this.goToSignUp}>Sign Up</button>
             </div>
         );
     }
