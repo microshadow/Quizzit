70b0b3c66aadc37bb0cd252d69b4fdb20abae72d
Jonathan Lehner <lehnerj@student.ethz.ch>
2018-11-07 20:38:47 -0500

    npm run build

--- quizzit/public/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//import Dashboard from './dashboard.js';
-import { STUDENT } from './globals.js';
-import WelcomeScreen from './welcomeScreen.js';
-
-var rootElem = React.createElement(WelcomeScreen, null);
-ReactDOM.render(rootElem, document.getElementById('root'));
--- quizzit/src/index.js
+++ quizzit/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 //import Dashboard from './dashboard.js';
 import { STUDENT } from './globals.js';
