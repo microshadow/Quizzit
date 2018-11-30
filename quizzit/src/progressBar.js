d306b5c9dbd37a73d8692849ba80a6ebac5bcdca
ahurka <hurka.alex@gmail.com>
2018-11-30 11:03:05 -0500

    Truncate progress bar display

--- quizzit/src/progressBar.js
+++ quizzit/src/progressBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './style/progressBar.css'
+import './style/statDisplays.css'
 
 class ProgressBar extends Component {
   constructor(props) {
@@ -17,7 +17,7 @@ class ProgressBar extends Component {
     const low  = percent < 50 ? baseZero : baseFifty;
     const high = percent < 50 ? baseFifty : baseHundred;
 
-    const percentDec = percent == 100 ? 1.0 : (percent % 50.0) / 50.0;
+    const percentDec = percent === 100 ? 1.0 : (percent % 50.0) / 50.0;
     for (let i = 0; i < 3; i++) {
       let avg = high[i] * percentDec + low[i] * (1.0 - percentDec);
       averageColor.push(avg);
@@ -28,7 +28,7 @@ class ProgressBar extends Component {
 
   render() {
     const percentFull = this.props.percent;
-    const barWidth = `${percentFull}%`;
+    const barWidth = `${percentFull.toFixed(2)}%`;
     const color = this.percentToColor(percentFull);
     const colorStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
 
